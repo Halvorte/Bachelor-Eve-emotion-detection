55d17534ba53a3d6aca63221a0462e07a9015776
Halvor <halvors@live.no>
2022-03-15 13:57:25 +0100

    Script to convert dataset annotations to yolo-format annotations

--- convert_csv_annotations_to_yolo.py
+++ convert_csv_annotations_to_yolo.py
@@ -4,8 +4,8 @@ import numpy as np
 from csv import reader
 
 
-directory = r'D:\Dataset\Automatically_annotated_file_list\Automatically_annotated_file_list\automatically_annotated.csv'                     # Set path for where to find annotations
-new_anno_path = r'D:\Dataset\Automatically_annotated_yolo'                  # Where to put new annotation files
+directory = r'D:\Dataset\Manually_Annotated_file_lists\validation.csv'                     # Set path for where to find annotations
+new_anno_path = r'D:\Dataset\Manually_annotated_yolo\validation'                  # Where to put new annotation files
 
 #f = open(directory)
 
@@ -14,26 +14,38 @@ with open(directory, 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Iterate over each row in the csv using reader object
+
+    # Remove the first line of the csv file
+    count = 0
     for row in csv_reader:
         # row variable is a list that represents a row in csv
         #print(row[0], row[6])
-
-        subDirectory_filepath = row[0]
-        if row[0] is not 'subDirectory_filepath':
+        if count == 0:
+            count += 1
+        else:
+            subDirectory_filepath = row[0]
             data = subDirectory_filepath.split('/')
-            print(data[1])
+            #print(data[1])
 
-        # Face location
-        #x_center = 0
-        #y_center = 0
-        #width = 0
-        #height = 0
+            # Face location
+            x_top_left = row[1]
+            y_top_left = row[2]
+            width = row[3]
+            height = row[4]
 
-        #
+            x_center = (int(width)//2) + int(x_top_left)
+            y_center = (int(height)//2) + int(y_top_left)
+            expression = row[6]
 
-        # Create a new file and write to it
-        #f = open(f"{new_anno_path}\{image}.txt", "w")
-        #f.write(f"{data[1]} {x_center} {y_center} {width} {height}")
-        #f.close()
+            image = data[1].strip('.jpg').strip('.JPG').strip('.pn').strip('.jpe')
 
-        #print(row)
+            # Drop the images with expression none, uncertain and non-face
+            if (expression == '8') or (expression == '9') or (expression == '10'):
+                print('8, 9 or 10')
+            else:
+                # Create a new file and write to it
+                f = open(f"{new_anno_path}\{image}.txt", "w")
+                f.write(f"{expression} {x_center} {y_center} {width} {height}")
+                f.close()
+
+                #print(f'{image} {x_center} {y_center} {width} {height} {expression}')
